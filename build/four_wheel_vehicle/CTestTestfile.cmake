b7f435e690e0079fa0198db1fe32fd4a335f4ab5
Yusuftaya1 <tayayusuf27@gmail.com>
2024-12-25 11:12:44 +0300

    Initial commit

---
+++ build/four_wheel_vehicle/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tayya/four_wheel_ws/src/four_wheel_vehicle
+# Build directory: /home/tayya/four_wheel_ws/build/four_wheel_vehicle
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/robot_bringup/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tayya/four_wheel_ws/src/robot_bringup
+# Build directory: /home/tayya/four_wheel_ws/build/robot_bringup
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
