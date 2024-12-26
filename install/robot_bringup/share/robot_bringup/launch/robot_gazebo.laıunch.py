9d4565b919f607972ac23fcbc00886c2bb82c283
Yusuftaya1 <tayayusuf27@gmail.com>
2024-12-26 18:04:21 +0300

    checked

---
+++ install/robot_bringup/share/robot_bringup/launch/robot_gazebo.laıunch.py
@@ -0,0 +1,60 @@
+import launch
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, FindPackageShare
+from launch_ros.actions import Node
+import os
+
+def generate_launch_description():
+    urdf_path = LaunchConfiguration('urdf_path', default=os.path.join(FindPackageShare('four_wheel_vehicle'), 'urdf', 'vehicle.urdf.xacro'))
+    rviz_config_path = LaunchConfiguration('rviz_config_path', default=os.path.join(FindPackageShare('robot_bringup'), 'rviz', 'gzb_rviiz.rviz'))
+
+    return LaunchDescription([
+        DeclareLaunchArgument('urdf_path', default_value=urdf_path, description='Path to the robot URDF'),
+        DeclareLaunchArgument('rviz_config_path', default_value=rviz_config_path, description='Path to the RViz config file'),
+
+        # Set use_sim_time parameter
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': launch.substitutions.Command(['xacro ', urdf_path]), 'use_sim_time': True}]
+        ),
+
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+            output='screen',
+            parameters=[{'use_sim_time': True}]
+        ),
+
+        # Include Gazebo launch file
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(FindPackageShare('gazebo_ros'), 'launch', 'gazebo.launch.py')
+            )
+        ),
+
+        # Spawn entity in Gazebo
+        Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            name='spawn_entity',
+            output='screen',
+            arguments=['-topic', 'robot_description', '-entity', 'vehicle'],
+            parameters=[{'use_sim_time': True}]
+        ),
+
+        # Start RViz
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', rviz_config_path],
+            parameters=[{'use_sim_time': True}]
+        ),
+    ])
