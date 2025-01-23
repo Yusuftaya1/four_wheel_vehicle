31c7b9632293c2cf9f6ff1e4336acf43454a8d8f
Yusuftaya1 <tayayusuf27@gmail.com>
2025-01-23 10:29:30 +0300

    working (i guess)

--- install/robot_bringup/share/robot_bringup/launch/a.py
+++ install/robot_bringup/share/robot_bringup/launch/a.py
@@ -1,6 +1,21 @@
-Node(
-    package='tf2_ros',
-    executable='static_transform_publisher',
-    name='static_transform_publisher',
-    arguments=['0', '0', '0', '0', '0', '0', 'map', 'odom']
-),
+ros2 topic pub /goal_pose geometry_msgs/msg/PoseStamped "{header: {frame_id: 'map'}, pose: {position: {x: 5.0, y: 3.0, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.0, w: 0.0}}}"
+
+
+ros2 topic pub /initialpose geometry_msgs/msg/PoseWithCovarianceStamped \
+"header:
+  frame_id: 'map'
+pose:
+  pose:
+    position: 
+      x: 0.0
+      y: 0.0
+      z: 0.0
+    orientation:
+      z: 0.0
+      w: 1.0
+  covariance: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 
+               0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 
+               0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 
+               0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 
+               0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 
+               0.0, 0.0, 0.0, 0.0, 0.0, 0.0]"
--- src/robot_bringup/launch/robot_joy_control.launch.py
+++ src/robot_bringup/launch/robot_joy_control.launch.py
@@ -44,7 +44,7 @@ def generate_launch_description():
             executable='static_transform_publisher',
             name='static_transform_publisher',
             arguments=['0', '0', '0', '0', '0', '0', 'map', 'odom']
-        ),
+        ),  
 
         Node(
             package='nav2_amcl',
